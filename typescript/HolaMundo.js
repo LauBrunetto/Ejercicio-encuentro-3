42698ccf11e3db3de9891b412cd1f0674973d8f2
Lautaro <laubrunetto1998@gmail.com>
2022-03-15 09:12:36 -0300

    commit ejercicios typescript

---
+++ typescript/HolaMundo.js
@@ -0,0 +1,9 @@
+function greet() {
+    return "Hola Mundo";
+}
+console.log(greet());
+function reversa(str) {
+    return str.split('').reverse().join('');
+}
+console.log(reversa('practica'));
+console.log(reversa('1 2 3 4 5 6'));
---
+++ typescript/HolaMundo.ts
@@ -0,0 +1,10 @@
+function greet():string {
+    return "Hola Mundo";
+}
+console.log(greet());
+
+function reversa(str:string): string{
+    return str.split('').reverse().join('')
+}
+console.log(reversa('practica'));
+console.log(reversa('1 2 3 4 5 6'));
